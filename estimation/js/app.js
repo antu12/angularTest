ae19bd5b8115e53eff76bf660d34c5bd1aa28bc3
Arshad <arshad.antu@gmail.com>
2017-09-25 02:56:49 +0600

    estimation of webDev

--- estimation/js/app.js
+++ estimation/js/app.js
@@ -2,53 +2,57 @@ var app = angular.module('myapp',["ngRoute","ngAnimate"]);
 
 app.config(function($routeProvider) {
 	$routeProvider
-		.when("/first",{
-			templateUrl:"partial/first.html",
-			controller:"pageCtrl"
-		})
-		.when("/second", {
-			templateUrl:"partial/second.html",
-			controller:"pageCtrl"
-		})
-		.when("/third", {
-			templateUrl:"partial/third.html",
-			controller:"pageCtrl"
-		})
-		.when("/fourth", {
-			templateUrl:"partial/fourth.html",
-			controller:"pageCtrl"
-		})
-		.when("/fifth", {
-			templateUrl:"partial/fifth.html",
-			controller:"pageCtrl"
-		})
-		.when("/sixth", {
-			templateUrl:"partial/sixth.html",
-			controller:"pageCtrl"
-		})
-		.when("/seventh", {
-			templateUrl:"partial/seventh.html",
-			controller:"pageCtrl"
-		})
-		.when("/eighth", {
-			templateUrl:"partial/eighth.html",
-			controller:"pageCtrl"
-		})
-		.when("/ninth", {
-			templateUrl:"partial/ninth.html",
-			controller:"pageCtrl"
-		})
-		.when("/landing", {
-			templateUrl:"partial/landing.html",
-			controller:"pageCtrl"
-		})
-		.when("/finish", {
-			templateUrl:"partial/finish.html",
-			controller:"pageCtrl"
-		})
-		.otherwise({
-			redirectTo:"/landing"
-		});
+	.when("/first",{
+		templateUrl:"partial/first.html",
+		controller:"pageCtrl"
+	})
+	.when("/second", {
+		templateUrl:"partial/second.html",
+		controller:"pageCtrl"
+	})
+	.when("/third", {
+		templateUrl:"partial/third.html",
+		controller:"pageCtrl"
+	})
+	.when("/fourth", {
+		templateUrl:"partial/fourth.html",
+		controller:"pageCtrl"
+	})
+	.when("/fifth", {
+		templateUrl:"partial/fifth.html",
+		controller:"pageCtrl"
+	})
+	.when("/sixth", {
+		templateUrl:"partial/sixth.html",
+		controller:"pageCtrl"
+	})
+	.when("/seventh", {
+		templateUrl:"partial/seventh.html",
+		controller:"pageCtrl"
+	})
+	.when("/eighth", {
+		templateUrl:"partial/eighth.html",
+		controller:"pageCtrl"
+	})
+	.when("/ninth", {
+		templateUrl:"partial/ninth.html",
+		controller:"pageCtrl"
+	})
+	.when("/landing", {
+		templateUrl:"partial/landing.html",
+		controller:"pageCtrl"
+	})
+	.when("/finish", {
+		templateUrl:"partial/finish.html",
+		controller:"pageCtrl"
+	})
+	.when("/webdev", {
+		templateUrl:"partial/webdev.html",
+		controller:"webdevCtrl"
+	})
+	.otherwise({
+		redirectTo:"/landing"
+	});
 });
 
 app.controller('headerCtrl', function($scope, $location) {
@@ -94,6 +98,23 @@ app.factory('priceService', function() {
 			choices[index]=choice;
 			// console.log('pushed '+ input);
 			// console.log(price);
+		},
+		estimate: function() {
+			total=0;
+			for (var i = 0; i < price.length; i++) {
+				total = total+price[i];
+			}
+			var estimate = "";
+			if (total >= 5000 && total <= 10000) {
+				estimate = "$"+total + " - $" + (total+590);
+			} else if (total > 10000 && total <= 15000) {
+				estimate = "$"+total + " - $" + (total+1090);
+			} else if (total > 15000 && total <= 20000) {
+				estimate = "$"+total + " - $" + (total+1290);
+			} else if (total > 20000 && total <= 25000) {
+				estimate = "$"+total + " - $" + (total+1390);
+			}
+			return estimate;
 		}
 	}
 });
@@ -104,6 +125,7 @@ app.controller('pageCtrl', function($scope, $location, priceService) {
 	$scope.price = priceService.getPrice();
 	$scope.allPrice = priceService.getAllPrice();
 	$scope.choices = priceService.getChoice();
+	$scope.estimate = priceService.estimate();
 	// console.log($scope.choices);
 
 	if ($scope.choices[0] != "start") {
@@ -123,5 +145,227 @@ app.controller('pageCtrl', function($scope, $location, priceService) {
 	$scope.reload = function() {
 		location.reload();
 	}
+
+});
+
+app.factory('webdevService', function() {
+	var features = [
+	{
+		name:"email login",
+		price:300,
+		icon:"glyphicon glyphicon-envelope",
+		status:false
+	},
+	{
+		name:"social login",
+		price:300,
+		icon:"glyphicon glyphicon-user",
+		status:false
+	},
+	{
+		name:"activity feed",
+		price:1300,
+		icon:"glyphicon glyphicon-thumbs-up",
+		status:false
+	},
+	{
+		name:"rating system",
+		price:600,
+		icon:"glyphicon glyphicon-star",
+		status:false
+	},
+	{
+		name:"multilingual",
+		price:800,
+		icon:"glyphicon glyphicon-globe",
+		status:false
+	},
+	{
+		name:"geo-location",
+		price:800,
+		icon:"glyphicon glyphicon-map-marker",
+		status:false
+	},
+	{
+		name:"custom UI",
+		price:800,
+		icon:"glyphicon glyphicon-eye-open",
+		status:false
+	},
+	{
+		name:"accept payments",
+		price:1000,
+		icon:"glyphicon glyphicon-credit-card",
+		status:false
+	},
+	{
+		name:"user profiles",
+		price:600,
+		icon:"glyphicon glyphicon-blackboard",
+		status:false
+	},
+	{
+		name:"messaging",
+		price:800,
+		icon:"glyphicon glyphicon-comment",
+		status:false
+	},
+	{
+		name:"maps",
+		price:500,
+		icon:"glyphicon glyphicon-screenshot",
+		status:false
+	},
+	{
+		name:"shopping cart",
+		price:2800,
+		icon:"glyphicon glyphicon-shopping-cart",
+		status:false
+	},
+	{
+		name:"notification",
+		price:500,
+		icon:"glyphicon glyphicon-ok-sign",
+		status:false
+	},
+	{
+		name:"search",
+		price:800,
+		icon:"glyphicon glyphicon-search",
+		status:false
+	},
+	{
+		name:"gallery",
+		price:300,
+		icon:"glyphicon glyphicon-picture",
+		status:false
+	},
+	{
+		name:"calander",
+		price:300,
+		icon:"glyphicon glyphicon-calendar",
+		status:false
+	},
+	{
+		name:"social sharing",
+		price:800,
+		icon:"glyphicon glyphicon-share",
+		status:false
+	},
+	{
+		name:"3rd-party API",
+		price:4800,
+		icon:"glyphicon glyphicon-hourglass",
+		status:false
+	},
+	{
+		name:"user privacy",
+		price:300,
+		icon:"glyphicon glyphicon-eye-close",
+		status:false
+	},
+	{
+		name:"sms intregation",
+		price:800,
+		icon:"glyphicon glyphicon-pencil",
+		status:false
+	},
+	{
+		name:"approval/moderation",
+		price:0,
+		icon:"glyphicon glyphicon-ban-circle",
+		status:false
+	},
+	{
+		name:"reporting",
+		price:500,
+		icon:"glyphicon glyphicon-alert",
+		status:false
+	},
+	{
+		name:"cms",
+		price:1800,
+		icon:"glyphicon glyphicon-equalizer",
+		status:false
+	},
+	{
+		name:"analytics",
+		price:600,
+		icon:"glyphicon glyphicon-stats",
+		status:false
+	},
+	{
+		name:"user administration",
+		price:500,
+		icon:"glyphicon glyphicon-modal-window",
+		status:false
+	},
+	{
+		name:"ticketing system",
+		price:800,
+		icon:"glyphicon glyphicon-bell",
+		status:false
+	},
+	{
+		name:"feedback system",
+		price:500,
+		icon:"glyphicon glyphicon-console",
+		status:false
+	},
+	{
+		name:"dashboard",
+		price:300,
+		icon:"glyphicon glyphicon-th-list",
+		status:false
+	},
+	];
+	return{
+		getitems: function() {
+			return features;
+		}
+	}
+
+});
+
+app.controller('webdevCtrl', function($scope, $location, webdevService) {
+	$scope.items = webdevService.getitems();
+	$scope.total=0;
+	// console.log($scope.items);
+	$scope.added = [];
+
+	$scope.toggle = function (item) {
+		
+		if (!item.status) {
+			$scope.add(item);
+			item.status = !item.status;
+		}
+		else{
+			$scope.remove(item);
+			item.status = !item.status;
+		}
+	}
+
+	$scope.add = function (item) {
+		$scope.added.push(item);
+		$scope.count();
+		console.log($scope.added);
+	};
+	$scope.remove = function (item) {
+		var index =$scope.added.indexOf(item);
+		$scope.added.splice(index, 1);
+		$scope.count();
+		console.log($scope.added);
+	};
+	$scope.count = function() {
+		var sum = 0;
+		for (var i = 0; i < $scope.added.length; i++) {
+			sum += $scope.added[i].price;
+		}
+		$scope.total = sum;
+	};
+	$scope.go = function(path) {
+		$location.path(path);
+	};
+
 
 });
